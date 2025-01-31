b12c3388bc2571aaccca3cbdb12116bbdae19fea
toggle-dev0 <munachienabulele2505@gmail.com>
2025-02-01 00:04:05 +0100

    Added cors package

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/response", (req, res) => {
   const data = {
     email: "munachienabulele2505@gmail.com",
     currentDateTime: new Date().toISOString(),
-    url: "someplace.com",
+    url: "https://github.com/toggle-dev0/Stage-zero-backend",
   };
   res.status(200).json(data);
 });
